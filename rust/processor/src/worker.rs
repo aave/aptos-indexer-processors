b6509b0cfafaadc8e97fc26db8f71d6c46449e23
Justin Chang <justin@aptoslabs.com>
2024-03-12 14:20:10 -0700

    stream

--- rust/processor/src/worker.rs
+++ rust/processor/src/worker.rs
@@ -47,7 +47,7 @@ use aptos_moving_average::MovingAverage;
 use futures::StreamExt;
 use kanal::AsyncSender;
 use std::sync::Arc;
-use tokio::{sync::RwLock, task::JoinHandle};
+use tokio::task::JoinHandle;
 use tracing::{debug, error, info};
 use url::Url;
 use warp::Filter;
@@ -66,10 +66,10 @@ async fn handle_websocket<
 >(
     websocket: warp::ws::WebSocket,
     query_params: AHashMap<String, String>,
-    cache: Arc<RwLock<C>>,
+    cache: Arc<C>,
 ) {
     let (tx, rx) = websocket.split();
-    let filter = Arc::new(RwLock::new(EventFilter::new()));
+    let filter = Arc::new(EventFilter::new());
 
     let start = query_params
         .get("start")
@@ -287,9 +287,7 @@ impl Worker {
         });
 
         if self.processor_config.name() == "event_stream_processor".to_string() {
-            let cache = Arc::new(RwLock::new(FIFOCache::<EventCacheKey, CachedEvent>::new(
-                1000000000,
-            )));
+            let cache = Arc::new(FIFOCache::<EventCacheKey, CachedEvent>::new(1000000000));
 
             // Add events to cache in order
             let cache_order = cache.clone();
